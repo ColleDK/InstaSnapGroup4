03508668a4680aaa004a1a9137f4ea1ca7b22bf9
ColleDK <s195490@student.dtu.dk>
2022-09-25 20:24:29 +0200

    Removed unused import

--- instasnap/src/screens/SignUpScreen.js
+++ instasnap/src/screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import {makeStyles} from "@mui/styles";
-import {Button, Divider, Grid, TextField} from "@mui/material";
+import {Divider, Grid, TextField} from "@mui/material";
 import {COLORS} from "../util/colors/Colors";
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
 import {SignUpAddPicture} from "../components/signup/SignUpAddPicture";
